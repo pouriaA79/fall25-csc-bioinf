444bd6a7a728f75cbe9ee009d3e60c3c10587ba6
pouriaA79 <80884677+pouriaA79@users.noreply.github.com>
2025-10-09 16:15:56 -0700

    Add files via upload

---
+++ week3/tests/test_upgma_python.py
@@ -0,0 +1,38 @@
+import sys, os
+
+# مسیر فایل جاری (مثلاً week3/tests/)
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+# مسیر src/
+src_dir = os.path.join(base_dir, "..", "src")
+
+# اضافه کردن مسیر src به sys.path
+if src_dir not in sys.path:
+    sys.path.append(src_dir)
+
+import time
+from phylo_python import upgma
+
+def test_upgma():
+    # D = [
+    #     [0.0, 5.0, 9.0, 9.0, 8.0],
+    #     [5.0, 0.0, 10.0, 10.0, 9.0],
+    #     [9.0, 10.0, 0.0, 8.0, 7.0],
+    #     [9.0, 10.0, 8.0, 0.0, 3.0],
+    #     [8.0, 9.0, 7.0, 3.0, 0.0],
+    # ]
+    D = [[abs(i - j) * 1.1 for j in range(100)] for i in range(100)]
+
+    tree = upgma(D)
+    d02 = tree.get_distance(0,2)
+    d34 = tree.get_distance(3,4)
+    print("UPGMA: dist(0,2)=%.3f, dist(3,4)=%.3f" %
+          (tree.get_distance(0, 2), tree.get_distance(3, 4)))
+
+if __name__ == "__main__":
+    t0 = time.perf_counter()
+    # test_upgma()
+    for _ in range(20):
+        test_upgma()
+    
+    print(f"Runtime: {int((time.perf_counter() - t0)*1000)}ms")
